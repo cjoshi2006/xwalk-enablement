eda040a64717d7cc3afcea98756b1e6a891a5297
cjoshi2006 <cjoshi2006@gmail.com>
2024-06-13 17:24:47 +0530

    changed name for the field

--- blocks/quote-nexa/quote-nexa.js
+++ blocks/quote-nexa/quote-nexa.js
@@ -1,7 +1,6 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement("blockquote");
-  //blockquote.textContent = quoteWrapper.textContent.trim()+"NEXA...";
-  blockquote.textContent ="NEXA";
+  blockquote.textContent ='NEXA';
   quoteWrapper.replaceChildren(blockquote);
 }
